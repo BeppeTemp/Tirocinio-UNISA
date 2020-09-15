625eb0684a0edbf527bac0088087f0b61bba1e4f
Giuseppe Arienzo <55873294+Tempesta58@users.noreply.github.com>
2020-09-15 21:18:21 +0200

    qere

--- Tirocinio_Arienzo_Unity/Assets/Script/RobotController.cs
+++ Tirocinio_Arienzo_Unity/Assets/Script/RobotController.cs
@@ -9,7 +9,12 @@ public class RobotController : MonoBehaviour
     public struct Joint
     {
         public string inputAxis;
-        public GameObject robotPart;
+        public GameObject robotPart_1;
+        public GameObject robotPart_2;
+        public GameObject robotPart_3;
+        public GameObject robotPart_4;
+        public GameObject robotPart_5;
+        public GameObject robotPart_6;
     }
     public Joint[] joints;
 
@@ -20,8 +25,18 @@ public class RobotController : MonoBehaviour
     {
         for (int i = 0; i < joints.Length; i++)
         {
-            GameObject robotPart = joints[i].robotPart;
-            UpdateRotationState(RotationDirection.None, robotPart);
+            GameObject robotPart_1 = joints[i].robotPart_1;
+            UpdateRotationState(RotationDirection.None, robotPart_1);
+            GameObject robotPart_2 = joints[i].robotPart_2;
+            UpdateRotationState(RotationDirection.None, robotPart_2);
+            GameObject robotPart_3 = joints[i].robotPart_3;
+            UpdateRotationState(RotationDirection.None, robotPart_3);
+            GameObject robotPart_4 = joints[i].robotPart_4;
+            UpdateRotationState(RotationDirection.None, robotPart_4);
+            GameObject robotPart_5 = joints[i].robotPart_5;
+            UpdateRotationState(RotationDirection.None, robotPart_5);
+            GameObject robotPart_6 = joints[i].robotPart_6;
+            UpdateRotationState(RotationDirection.None, robotPart_6);
         }
     }
 
@@ -29,7 +44,12 @@ public class RobotController : MonoBehaviour
     {
         StopAllJointRotations();
         Joint joint = joints[jointIndex];
-        UpdateRotationState(direction, joint.robotPart);
+        UpdateRotationState(direction, joint.robotPart_1);
+        UpdateRotationState(direction, joint.robotPart_2);
+        UpdateRotationState(direction, joint.robotPart_3);
+        UpdateRotationState(direction, joint.robotPart_4);
+        UpdateRotationState(direction, joint.robotPart_5);
+        UpdateRotationState(direction, joint.robotPart_6);
     }
 
     // HELPERS
